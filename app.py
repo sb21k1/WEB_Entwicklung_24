51c219799d97836b7a2a313ce0c46f358c748453
sb21k1 <157414915+sb21k1@users.noreply.github.com>
2024-05-14 13:16:20 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+#später hier DB anbindung
+locations = [
+    {"name": "Location1", "description": "Beschreibung1"},
+    {"name": "Location2", "description": "Beschreibung2"},
+    {"name": "Location3", "description": "Beschreibung3"},
+    {"name": "Location1", "description": "Beschreibung1"},
+    {"name": "Location2", "description": "Beschreibung2"},
+    {"name": "Location3", "description": "Beschreibung3"},
+    {"name": "Location1", "description": "Beschreibung1"},
+    {"name": "Location2", "description": "Beschreibung2"},
+    {"name": "Location3", "description": "Beschreibung3"},
+    {"name": "Location1", "description": "Beschreibung1"},
+    {"name": "Location2", "description": "Beschreibung2"},
+    {"name": "Location3", "description": "Beschreibung3"}
+    
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', locations = locations)
