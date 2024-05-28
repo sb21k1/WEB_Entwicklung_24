50845aecf47f8463fb3b1f4cae9d5e394e5719c8
sb21k1 <157414915+sb21k1@users.noreply.github.com>
2024-05-28 22:34:45 +0200

    Version 1.02.02 (#6)
    hoffe das ich die änderungen dann aus der main ziehen kann

--- app.py
+++ app.py
@@ -20,7 +20,15 @@ locations = [
     {"name": "Location3", "description": "Beschreddibung3"}
     
 ]
+theme_tags = [
+    {"name": "urban"}, {"name": "park"},{"name": "café"},{"name": "restaurant"},{"name": "library"},{"name": "museum"},{"name": "historical"},{"name": "modern"},
+    {"name": "bridge"},{"name": "street"},{"name": "train station"},{"name": "subway"},{"name": "harbor"},{"name": "rooftop"},{"name": "garden"},
+    {"name": "castle"},{"name": "market"},{"name": "warehouse"},{"name": "studio"},{"name": "theater"},{"name": "concert hall"},{"name": "mall"},{"name": "temple"},
+    {"name": "church"},{"name": "monument"},{"name": "ruins"},{"name": "hotel"},{"name": "university"},{"name": "office"},
+    {"name": "zoo"}
+]
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', locations = locations)
+    return render_template('index.html', locations = locations, theme_tags = theme_tags)
